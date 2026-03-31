3bcd6a856407a29b03449585c98cee49344a03f8
Hector Lozano <TU_CORREO_DE_GITHUB>
2026-03-30 22:00:20 -0500

    Fix static serving path for production

--- server/server.ts
+++ server/server.ts
@@ -671,10 +671,10 @@ app.use(vite.middlewares);
 
 }else{
 
-app.use(express.static(path.join(__dirname,"dist")));
+app.use(express.static(path.join(__dirname,"../dist")));
 
 app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"dist","index.html"));
+res.sendFile(path.join(__dirname,"../dist","index.html"));
 });
 
 }
